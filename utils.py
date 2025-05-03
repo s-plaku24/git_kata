90da36c5fc8e38fac02ad637570f67c1156265bd
s-plaku24 <s_plaku24@stud.hwr-berlin.de>
2025-05-03 18:18:47 +0200

    Load titanic data in utils branch

---
+++ utils.py
@@ -0,0 +1,3 @@
+import pandas as pd
+def load_data():
+    df = pd.read_csv(r'C:\Users\Sebastiano Plaku\OneDrive\Desktop\HWR\Subjects\SoSe2025\Enterprise Architecture for Big Data\git_kata\git_kata\data\titanic.csv')
