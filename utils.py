6ffdf507e56cbf87b034029c774b010e95cbc83f
s-plaku24 <s_plaku24@stud.hwr-berlin.de>
2025-05-03 18:28:41 +0200

    Add jupyter notebook

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
 import pandas as pd
 def load_data():
-    df = pd.read_csv(r'C:\Users\Sebastiano Plaku\OneDrive\Desktop\HWR\Subjects\SoSe2025\Enterprise Architecture for Big Data\git_kata\git_kata\data\titanic.csv')
+    file_path = r"C:\Users\Sebastiano Plaku\OneDrive\Desktop\HWR\Subjects\SoSe2025\Enterprise Architecture for Big Data\git_kata\git_kata\data\titanic.csv"
+    return pd.read_csv(file_path)
