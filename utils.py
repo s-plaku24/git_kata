836bfef0b6d715d2380e2ca61fc54421541a70bd
s-plaku24 <s_plaku24@stud.hwr-berlin.de>
2025-05-03 19:08:48 +0200

    final_push

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def load_data():
 
 def clean_data(df):
     """ Cleans the DataFrame by dropping rows with missing values and converting categorical columns to lowercase. """
-    """df = df.dropna()"""
+    "df = df.dropna()" # Drop rows with missing values, if we dont comment this out then dataframe will be empty
 
     categorical_columns = df.select_dtypes(include=['object']).columns
     for col in categorical_columns:
